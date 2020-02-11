829e7d25898bf593a1bc793a006ba13809a705e1
Johnson Kow <jkow95@gmail.com>
2020-02-11 04:12:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon (path, emoticon)
   result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(path, )
   # code goes here
 end
 
